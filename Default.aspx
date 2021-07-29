31c52ca88c3b7635f370997a691f68538cd342d8
Ivon-cpp <vania9023@gmail.com>
2021-07-29 17:08:09 +0300

    Исходная версия - с главной страницей

---
+++ Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="Главная" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestSite._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <!-- 1. -->
+    <div class="header-box">
+        <h1 class="fw-light">Почта-Дрон</h1>
+        <p class="lead text-muted">Доставка будущего</p>
+        <p>
+          <a href="#" class="btn btn-primary my-btn-1">Вход</a>
+          <a href="#" class="btn btn-secondary my-btn-2">Регистрация</a>
+        </p> 
+      </div>
+
+    <hr />
+
+    <div class="album container">
+        <div class="row">
+            <% for (int i = 0; i <= 9; i++) { %>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-top">
+                        <img src ="Images/logo-1.png" alt="..." />
+                    </div>
+                    <div class ="card-body">
+
+                    </div>
+                </div>
+            </div>
+            <% } %>
+        </div>
+    </div>
+    
+
+</asp:Content>
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("TestSite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSite")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("06a388e7-8688-453b-8d8c-ab24304b3d52")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
